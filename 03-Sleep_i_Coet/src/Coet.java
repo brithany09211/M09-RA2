a3d186ff2f855a687862ba744955de4c5d6eef27
brithany09211 <smb09211@gmail.com>
2026-01-19 17:12:11 +0100

    03-Sleep_i_Coet Terminada correctamente las clases Motor y Coet

--- 03-Sleep_i_Coet/src/Coet.java
+++ 03-Sleep_i_Coet/src/Coet.java
@@ -1,3 +1,56 @@
+import java.util.Scanner;
+
 public class Coet {
-    
+    public static void passarAPotencia(int p, Motor[] motors) {
+        if (p < 0 || p > 10) {
+            System.out.println("Error: La potència ha d'estar entre 0 i 10");
+            return;
+        }
+        
+        System.out.println("Passant a potència " + p);
+        for (Motor m : motors) {
+            m.setPotencia(p);
+        }
+    }
+
+    public static void main(String[] args) {
+        Motor[] motors = new Motor[4];
+        for (int i = 0; i < 4; i++) {
+            motors[i] = new Motor("Motor " + i);
+            motors[i].start();
+        }
+
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            if (sc.hasNextInt()) {
+                int p = sc.nextInt();
+                passarAPotencia(p, motors);
+                
+                if (p == 0) {
+                    // Espera a que tots els motors arribin a 0
+                    boolean totsCero = false;
+                    while (!totsCero) {
+                        try {
+                            Thread.sleep(100);
+                            totsCero = true;
+                            for (Motor m : motors) {
+                                if (m.getPotencia() != 0) {
+                                    totsCero = false;
+                                    break;
+                                }
+                            }
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        
+        sc.close();
+        System.out.println("0");
+        System.exit(0);
+    }
 }
--- 03-Sleep_i_Coet/src/Motor.java
+++ 03-Sleep_i_Coet/src/Motor.java
@@ -1,3 +1,57 @@
-public class Motor {
+public class Motor extends Thread {
+    private int potencia = 0;
+    private int objectiu = 0;
+    private boolean actiu = false;
+
+    public Motor(String nom) {
+        super(nom);
+    }
+
+    public synchronized void setPotencia(int p) {
+        this.objectiu = p;
+        this.actiu = true; 
+    }
+
     
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public int getObjectiu() {
+        return objectiu;
+    }
+
+    public boolean isActiu() {
+        return actiu;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                if (actiu) {
+                    String accio;
+                    if (potencia < objectiu) {
+                        potencia++;
+                        accio = "Incre.";
+                    } else if (potencia > objectiu) {
+                        potencia--;
+                        accio = "Decre.";
+                    } else {
+                        accio = "FerRes";
+                        actiu = false; 
+                    }
+
+                    System.out.println(getName() + ": " + accio + " Objectiu: " + objectiu + " Actual: " + potencia);
+
+                    Thread.sleep(800 + (int)(Math.random() * 700));
+                } else {
+                    Thread.sleep(100); 
+                }
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
 }
