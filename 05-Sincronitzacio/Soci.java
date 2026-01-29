b971640efaec2dad3e4514c85a7bd60e773dec88
brithany09211 <smb09211@gmail.com>
2026-01-29 19:49:17 +0100

    05-Sincronitzacio- synchronized en funcionamiento valor total 0

--- 05-Sincronitzacio/Soci.java
+++ 05-Sincronitzacio/Soci.java
@@ -6,6 +6,7 @@ public class Soci extends Thread {
     private int aportacio;
     private int esperaMax;
     private Random random;
+    private int maxAnys;
     
     public Soci(String nom, Compte compte, int aportacio, int esperaMax) {
         this.nom = nom;
@@ -13,6 +14,7 @@ public class Soci extends Thread {
         this.aportacio = aportacio;
         this.esperaMax = esperaMax;
         this.random = new Random();
+        this.maxAnys = 10; // Per defecte
     }
     
     @Override
@@ -20,12 +22,16 @@ public class Soci extends Thread {
         compte.ingres(aportacio);
         
         try {
-            int delay = random.nextInt(esperaMax);
+            int delay = random.nextInt(esperaMax + 1);
             Thread.sleep(delay);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         
-        compte.treu(10);
+        compte.treu(aportacio);
+    }
+    
+    public Compte getCompte() {
+        return compte;
     }
 }
